a90650f328f6e7ddbbe0b5084be296c391a5879a
gabis <gabisovmaxim@gmail.com>
2025-03-19 13:13:04 +0300

    Classes for ORM

--- Database/Classes.py
+++ Database/Classes.py
@@ -1,3 +1,4 @@
+import datetime
 import sqlalchemy
 import sqlalchemy.orm as orm
 import sqlalchemy.types as satypes
@@ -24,6 +25,8 @@ class Product(Base):
 class Promocode(Base):
     __tablename__ = 'Promocodes'
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True, autoincrement=True)
+    name: orm.Mapped[str] = orm.mapped_column(satypes.String(30))
+    expiration_date: orm.Mapped[satypes.DateTime] = orm.mapped_column(default=datetime.datetime.now())
 
 class Order(Base):
     __tablename__ = 'Orders'
