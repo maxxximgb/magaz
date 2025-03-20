c0605dbba211159fc1a1fb9196022b4a097bf94e
maxxximgb <kruzakoalsl@gmail.com>
2025-03-20 18:07:02 +0300

    Create Product.py

---
+++ Database/Product.py
@@ -0,0 +1,13 @@
+import datetime
+import sqlalchemy
+import sqlalchemy.orm as orm
+import sqlalchemy.types as satypes
+from Database.Manager import Base
+
+class Product(Base):
+    __tablename__ = 'Products'
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True, autoincrement=True)
+    name: orm.Mapped[str] = orm.mapped_column(satypes.String(30))
+    weight: orm.Mapped[str] = orm.mapped_column(satypes.String(30))
+    order_id: orm.Mapped[int] = orm.mapped_column(sqlalchemy.ForeignKey('Orders.id'))
+    order: orm.Mapped["Order"] = orm.relationship("Order", back_populates="products")
