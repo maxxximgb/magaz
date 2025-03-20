9392e0978211b29b906711aed2f6f20f9e19d14e
maxxximgb <kruzakoalsl@gmail.com>
2025-03-20 18:08:11 +0300

    Create Order.py

---
+++ Database/Order.py
@@ -0,0 +1,13 @@
+import datetime
+import sqlalchemy
+import sqlalchemy.orm as orm
+import sqlalchemy.types as satypes
+from Database.Manager import Base
+
+class Order(Base):
+    __tablename__ = 'Orders'
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True, autoincrement=True)
+    weight: orm.Mapped[str] = orm.mapped_column(satypes.String(30))
+    products: orm.Mapped[list[Product]] = orm.relationship("Product", back_populates="order")
+    promocode_id: orm.Mapped[int] = orm.mapped_column(sqlalchemy.ForeignKey('Promocodes.id'), nullable=True)
+    promocode: orm.Mapped[Promocode] = orm.relationship("Promocode")
