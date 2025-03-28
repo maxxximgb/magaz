a997502e840959c65f3a27de31e5d731ee41f680
gabis <gabisovmaxim@gmail.com>
2025-03-28 16:42:15 +0300

    Server routes created and applied on base.html

--- server.py
+++ server.py
@@ -9,4 +9,16 @@ engine.create_session()
 def index():
     return render_template('base.html')
 
-app.run(port=8080)
+@app.route('/production', methods=["GET"])
+def production():
+    return render_template('base.html')
+
+@app.route('/about', methods=["GET"])
+def about_us():
+    return render_template('base.html')
+
+@app.route('/buy', methods=["GET"])
+def buy():
+    return render_template('base.html')
+
+app.run(host='0.0.0.0', port=8080)
