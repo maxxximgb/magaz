f81cee4c958b4c42ecfe05089a67198473191996
Eva <tsorievaeva@yandex.ru>
2025-03-29 01:08:08 +0300

    Добавление файлов

--- server.py
+++ server.py
@@ -7,18 +7,18 @@ engine.create_session()
 
 @app.route('/', methods=["GET"])
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 @app.route('/production', methods=["GET"])
 def production():
-    return render_template('base.html')
+    return render_template('index.html')
 
 @app.route('/about', methods=["GET"])
 def about_us():
-    return render_template('base.html')
+    return render_template('index.html')
 
 @app.route('/buy', methods=["GET"])
 def buy():
-    return render_template('base.html')
+    return render_template('index.html')
 
 app.run(host='0.0.0.0', port=8080)
